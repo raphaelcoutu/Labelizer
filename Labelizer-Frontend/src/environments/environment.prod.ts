2273c5d1b075c7e3b180b461eb524ecfb978bf11
Raphaël Coutu <raph244@gmail.com>
2020-08-10 07:00:52 -0400

    F: set env urls

--- Labelizer-Frontend/src/environments/environment.prod.ts
+++ Labelizer-Frontend/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  backendUrl: 'http://pharmacie-dev.reg05.rtss.qc.ca:8000/labelizer-backend',
+  apiUrl: 'http://pharmacie-dev.reg05.rtss.qc.ca:8000/labelizer-backend/api'
 };
